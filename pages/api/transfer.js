861ad927d6daa6040ae6b298b54eb933d4dd206c
luka1104 <skyward.luke11@gmail.com>
2022-06-05 03:44:41 -0700

    simplify transfer conde

--- pages/api/transfer.js
+++ pages/api/transfer.js
@@ -13,6 +13,25 @@ const contract = require('../../src/contracts/BeanToken.json');
 const tokenAddress = process.env.TOKEN_ADDRESS;
 const Contract = new web3.eth.Contract(contract.abi, tokenAddress, { from: PUBLIC_KEY });
 
+const TransferToken = async (addr1, addr2) => {
+  const data = await Contract.methods.multiTransfer([addr1, addr2], [100000000, 100000000]).encodeABI();
+  const tx = {
+    "gas": 500000,
+    "to": tokenAddress,
+    "value": "0x00",
+    "data": data,
+    "from": PUBLIC_KEY
+  };
+  web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, (err, signedTx) => {
+    if (err) return err
+    console.log(signedTx)
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction, (err, res) => {
+      if (err) return console.log(err)
+      console.log(res)
+    });
+  });
+};
+
 const handler = async (req, res) => {
   const now = Date.now();
   
@@ -21,7 +40,6 @@ const handler = async (req, res) => {
   for (const [a, t] of Object.entries(connections)) {
     console.log(`address: ${a} time: ${t}`);
     if (t > now - 20000) { // if connection time is within last 20 seconds
-      console.log('collect');
       // We found the connection
       found = a;
       delete connections[a]; 
@@ -29,36 +47,11 @@ const handler = async (req, res) => {
     }
   }
 
-  const TransferToken = async () => {
-    const data = await Contract.methods.multiTransfer([found, req.body], [100000000, 100000000]).encodeABI()
-    const tx = {
-      "gas": 500000,
-      "to": tokenAddress,
-      "value": "0x00",
-      "data": data,
-      "from": PUBLIC_KEY
-    };
-    web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, (err, signedTx) => {
-      if (err) {
-        return err
-      } else {
-        console.log(signedTx)
-        web3.eth.sendSignedTransaction(signedTx.rawTransaction, (err, res) => {
-          if (err) {
-            console.log(err)
-          } else {
-            console.log(res)
-          }
-        })
-      }
-    });
-  }
   if (found) {
-    await TransferToken()
+    await TransferToken(found, req.body)
   } else {
     connections[req.body] = now;
   }
-  console.log(`connect ${Object.entries(connections)}`);
-  return res
+  res.status(200).send("success");
 };
 export default handler;
