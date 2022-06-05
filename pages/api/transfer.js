ece97c0b7872755f4828e3031c5e0fb2cc5178a9
luka1104 <skyward.luke11@gmail.com>
2022-06-05 04:56:58 -0700

    test

--- pages/api/transfer.js
+++ pages/api/transfer.js
@@ -1,7 +1,20 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
 import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, getDocs, setDoc } from "firebase/firestore/lite";
 
-const connections = {"0x50B80aa3877fC852f3194a0331177FDDcF0891bf": Date.now()};
+const firebaseConfig = {
+  apiKey: "AIzaSyDPnRC0VToZsRcoXuDG7xq9alr1fXhi_g8",
+  authDomain: "meetn-9cc66.firebaseapp.com",
+  projectId: "meetn-9cc66",
+  storageBucket: "meetn-9cc66.appspot.com",
+  messagingSenderId: "774863347933",
+  appId: "1:774863347933:web:3808cdbcf47ab0a785bb34"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// const connections = {"0x50B80aa3877fC852f3194a0331177FDDcF0891bf": Date.now()};
 
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -13,9 +26,23 @@ const contract = require('../../src/contracts/BeanToken.json');
 const tokenAddress = process.env.TOKEN_ADDRESS;
 const Contract = new web3.eth.Contract(contract.abi, tokenAddress, { from: PUBLIC_KEY });
 
+const getConnections = async (db) => {
+  const conCol = collection(db, 'connections');
+  const conSnap = await getDocs(conCol);
+  const conList = conSnap.docs.map(doc => doc.data());
+  const connections = {};
+  conList.forEach(con => connections[con.address] = con.timestamp);
+  return connections;
+};
+
+const addConnection = async (address, timestamp) => {
+  await setDoc(doc(db, 'connections', address), {
+    address: address,
+    timestamp: timestamp
+  });
+};
+
 const TransferToken = async (addr1, addr2) => {
-  console.log(PUBLIC_KEY);
-  console.log(addr1, addr2);
   const data = await Contract.methods.multiTransfer([addr1, addr2], [100000000, 100000000]).encodeABI();
   const tx = {
     "gas": 500000,
@@ -25,10 +52,7 @@ const TransferToken = async (addr1, addr2) => {
     "from": PUBLIC_KEY
   };
   await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, async (err, signedTx) => {
-    if (err) {
-      console.log(err);
-      return err;
-    }
+    if (err) return err;
     console.log(signedTx)
     await web3.eth.sendSignedTransaction(signedTx.rawTransaction, (err, res) => {
       if (err) return console.log(err)
@@ -38,8 +62,9 @@ const TransferToken = async (addr1, addr2) => {
 };
 
 const handler = async (req, res) => {
+  await addConnection(req.body, Date.now());
+  const connections = await getConnections(db);
   const now = Date.now();
-  console.log('PUB', PUBLIC_KEY);
   console.log('CONNECTIONS', connections); 
   console.log('REQ.BODY', req.body);
   
@@ -50,19 +75,16 @@ const handler = async (req, res) => {
     if (t > now - 20000) { // if connection time is within last 20 seconds
       // We found the connection
       found = a;
-      console.log('found', found);
       delete connections[a]; 
       break;
     }
   }
 
   if (found) {
-    console.log('transfering');
     await TransferToken(found, req.body)
   } else {
-    console.log('adding');
     connections[req.body] = now;
-    // res.status(200).send("success");
   }
+  res.status(200).send("success");
 };
 export default handler;
